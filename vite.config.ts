e3980e2d316a00bae36734021d666d614cdbe0a3
k s <131339220+XiaoyuBook@users.noreply.github.com>
2026-04-05 15:09:42 +0800

    Make Vite base path configurable

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,33 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
-    assetsInclude: ["**/*.bin"],
-    plugins: [
-        react(),
-        VitePWA({
-            registerType: "autoUpdate",
-            workbox: {
-                maximumFileSizeToCacheInBytes: 8000000,
-            },
-            devOptions: {
-                enabled: true,
-            },
-            includeAssets: ["**/*.bin"],
-            manifest: {
-                name: "Ten Lines",
-                short_name: "Ten Lines",
-                description:
-                    "Suite of tools for RNG Manipulation in Generation 3",
-                theme_color: "#79D4F5",
-            },
-        }),
-    ],
-    build: {
-        minify: true,
-    },
-    base: "/ten-lines/",
-}));
+export default defineConfig(() => {
+    const base = process.env.TEN_LINES_BASE || "/";
+    return {
+        assetsInclude: ["**/*.bin"],
+        plugins: [
+            react(),
+            VitePWA({
+                registerType: "autoUpdate",
+                workbox: {
+                    maximumFileSizeToCacheInBytes: 8000000,
+                },
+                devOptions: {
+                    enabled: true,
+                },
+                includeAssets: ["**/*.bin"],
+                manifest: {
+                    name: "Ten Lines",
+                    short_name: "Ten Lines",
+                    description:
+                        "Suite of tools for RNG Manipulation in Generation 3",
+                    theme_color: "#79D4F5",
+                },
+            }),
+        ],
+        build: {
+            minify: true,
+        },
+        base,
+    };
+});
