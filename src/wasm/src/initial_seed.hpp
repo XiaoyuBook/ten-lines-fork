a5b52e9c42ab2f0259d484c3d5fac39fe5abe439
XiaoyuBook <131339220+XiaoyuBook@users.noreply.github.com>
2026-05-27 16:12:17 +0800

    fix:修复Switch日版FRLG初始种子配置

--- src/wasm/src/initial_seed.hpp
+++ src/wasm/src/initial_seed.hpp
@@ -260,11 +260,33 @@ static inline std::map<std::string, std::vector<HeldButtonOffset>> HELD_BUTTON_O
             { 'a', "none", 0 },
 
             { 'h', "none", 0 },
+            { 'h', "blackout_r", -36 },
+            { 'h', "blackout_l", -36 },
+
+            { 'r', "none", 0 },
+        } },
+
+    { "fr_jpn_nx",
+        {
+            { 'a', "none", 0 },
+
+            { 'h', "none", 0 },
 
             { 'r', "none", 0 },
         } },
 
     { "lg_nx",
+        {
+            { 'a', "none", 0 },
+
+            { 'h', "none", 0 },
+            { 'h', "blackout_r", -36 },
+            { 'h', "blackout_l", -36 },
+
+            { 'r', "none", 0 },
+        } },
+
+    { "lg_jpn_nx",
         {
             { 'a', "none", 0 },
 
